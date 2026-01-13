6147d73b748fe68e217b0e79acbe32d35b79d896
Kristian Veter <kristian.v.dev@gmail.com>
2026-01-13 13:54:21 +0200

    feature: implement hubspot chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,11 +40,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${inter.className} ${jetbrainsMono.variable} font-sans antialiased`}
       >
         {children}
         <Analytics />
+
+        <Script
+          id="hubspot-chat"
+          src="https://js-na3.hs-scripts.com/342281824.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
